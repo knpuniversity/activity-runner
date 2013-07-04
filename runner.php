60e1f270b6c5b3e80dc441407bd633a62817dee8
Kristen Gilden <kristen.gilden@knplabs.com>
2013-07-04 14:13:17 -0700

    Typo fix

--- runner.php
+++ runner.php
@@ -3,7 +3,7 @@
 
 if (is_file($autoloadFile = __DIR__.'/vendor/autoload.php')) {
     // Installed as a standalone project
-} else if (is_file($autoloadFIle = __DIR__.'/../../autoload.php')) {
+} else if (is_file($autoloadFile = __DIR__.'/../../autoload.php')) {
     // Installed as a library
 } else {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install"?');
