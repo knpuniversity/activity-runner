5fc942e3d25e189b07fb5ef19e93b184f624cf30
Kristen Gilden <kristen.gilden@knplabs.com>
2013-07-04 13:53:20 -0700

    Fixing runner failing when installed as a library

--- runner.php
+++ runner.php
@@ -1,7 +1,15 @@
 #!/usr/bin/env php
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+if (is_file($autoloadFile = __DIR__.'/vendor/autoload.php')) {
+    // Installed as a standalone project
+} else if (is_file($autoloadFIle = __DIR__.'/../../autoload.php')) {
+    // Installed as a library
+} else {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install"?');
+}
+
+require $autoloadFile;
 
 use KnpU\ActivityRunner\Application;
 
