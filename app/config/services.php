knpuniversity/activity-runner

commit 1ca2b27d224f025632099ce59edfe777bf39e7b1

Adding timeout limit to PhpWorker
Some more tests were also written to ensure the worker does not run
infinitely.

app/config/services.php
@@ -99,7 +99,8 @@ $app['worker.chained'] = $app->share(function ($app) {
 $app['worker.php'] = $app->share(function ($app) {
     return new KnpU\ActivityRunner\Worker\PhpWorker(
         $app['filesystem'],
-        $app['php_parser']
+        $app['php_parser'],
+        $app['worker.time_limit']
     );
 });
 

app/config/parameters.php.dist
@@ -4,4 +4,7 @@
 // search for activity configurations. It may be a string or an array.
 $app['courses_path'] = __DIR__.'/../courses/';
 
+// Maximum amount of time in seconds a worker can take to execute user code.
+$app['worker.time_limit'] = 10;
+
 return $app;

src/KnpU/ActivityRunner/Tests/Worker/PhpWorkerTest.php
@@ -11,6 +11,13 @@ use Symfony\Component\Filesystem\Filesystem;
  */
 class PhpWorkerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * The prefix used for environments to the /tmp directory.
+     *
+     * @var string
+     */
+    protected $prefix = 'activity_runner_test_';
+
     public function testWorkerExecutesPhpFiles()
     {
         $base = <<<EOD
@@ -32,15 +39,110 @@ EOD;
 
         $activity = $this->getMockActivity($files, 'base.php');
 
-        $worker = new PhpWorker(new Filesystem(), $this->getMockParser());
+        $worker = $this->createWorker();
+
         $result = $worker->render($activity);
 
         $this->assertContains('Hello, world!', $result->getOutput());
     }
 
-    public function testSupportsReturnsTrueIfNotPhp()
+    public function testLanguageErrorIfTimeOutReached()
     {
-        $worker = new PhpWorker(new Filesystem(), $this->getMockParser());
+        $code  = '<?php while (true);';
+        $files = new ArrayCollection(array('index.php' => $code));
+
+        $activity = $this->getMockActivity($files, 'index.php');
+
+        $worker = $this->createWorker();
+        $worker->setTimeout(0.2);
+
+        $result = $worker->render($activity)->toArray();
+
+        $this->assertContains('too long time', $result['errors']['language']);
+    }
+
+    public function testChildProcessTerminatedIfTimeoutReached()
+    {
+        $code  = '<?php while (true);';
+        $files = new ArrayCollection(array('index.php' => $code));
+
+        $activity = $this->getMockActivity($files, 'index.php');
+
+        $mockFs = $this->getMockFilesystem();
+
+        $mockFs
+            ->expects($this->once())
+            ->method('dumpFile')
+            ->will($this->returnCallback(function ($filename, $content) {
+                $filesystem = new Filesystem();
+                $filesystem->dumpFile($filename, $content);
+            }))
+        ;
+
+        // The critical expectation for this test to pass.
+        $mockFs
+            ->expects($this->once())
+            ->method('remove')
+        ;
+
+        $worker = $this->createWorker($mockFs);
+        $worker->setTimeout(0.2);
+        $worker->render($activity);
+    }
+
+    public function testNoChildProcessesLeftDangling()
+    {
+        $code  = '<?php while (true);';
+        $files = new ArrayCollection(array('index.php' => $code));
+
+        $activity = $this->getMockActivity($files, 'index.php');
+
+        // Search for running processes that have a "activity_runner_test" in it.
+        exec('ps aux | grep [a]ctivity_runner_test', $output);
+        $runningScriptCount = count($output);
+
+        $worker = $this->createWorker();
+        $worker->setTimeout(0.2);
+        $worker->render($activity);
+
+        // The worker should have killed the newly created process. Let's look
+        // at running processes again - we should have exactly the same number
+        // of them as prior to starting the worker.
+        exec('ps aux | grep [a]ctivity_runner_test', $output);
+
+        $this->assertCount($runningScriptCount, $output);
+    }
+
+    /**
+     * @group ini
+     */
+    public function testChangingIniSettingsHasNoEffectOnTimeout()
+    {
+        $code = <<<EOD
+<?php
+
+ini_set('max_execution_time', 2);
+while (true);
+EOD;
+
+        $files = new ArrayCollection(array('index.php' => $code));
+
+        $activity = $this->getMockActivity($files, 'index.php');
+
+
+        $worker = $this->createWorker();
+        $worker->setTimeout(0.2);
+
+        $timeStart = microtime(true);
+        $worker->render($activity);
+        $timeStop = microtime(true);
+
+        $this->assertEquals(0.2, $timeStop - $timeStart, '', 0.05);
+    }
+
+    public function testSupportsReturnsTrueIfPhp()
+    {
+        $worker = $this->createWorker();
 
         $this->assertTrue($worker->supports('foo.php', array()));
     }
@@ -52,7 +154,7 @@ EOD;
      */
     public function testSupportsReturnsFalseIfNotPhp($fileName)
     {
-        $worker = new PhpWorker(new Filesystem(), $this->getMockParser());
+        $worker = $this->createWorker();
 
         $this->assertFalse($worker->supports($fileName, array()));
     }
@@ -65,6 +167,31 @@ EOD;
             array('baz.xml'),
             array('php'),
         );
+    }
+
+    protected function tearDown()
+    {
+        $filesystem  = new Filesystem();
+        $directories = glob(sys_get_temp_dir().'/'.$this->prefix.'*');
+
+        foreach ($directories as $directory) {
+            $filesystem->remove($directory);
+        }
+    }
+
+    /**
+     * Creates a new worker. Used for keeping the tests compact.
+     *
+     * @param Filesystem|null $filesystem
+     *
+     * @return PhpWorker
+     */
+    private function createWorker(Filesystem $filesystem = null)
+    {
+        $worker = new PhpWorker($filesystem ?: new Filesystem(), $this->getMockParser());
+        $worker->setPrefix($this->prefix);
+
+        return $worker;
     }
 
     /**
@@ -90,6 +217,14 @@ EOD;
         ;
 
         return $activity;
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockFilesystem()
+    {
+        return $this->getMock('Symfony\\Component\\Filesystem\\Filesystem');
     }
 
     /**

src/KnpU/ActivityRunner/Worker/PhpWorker.php
@@ -8,6 +8,7 @@ use KnpU\ActivityRunner\ActivityInterface;
 use KnpU\ActivityRunner\Assert\AssertSuite;
 use KnpU\ActivityRunner\Assert\PhpAwareInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\RuntimeException;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
 
@@ -27,6 +28,18 @@ class PhpWorker implements WorkerInterface
     protected $parser;
 
     /**
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * The maximum amount of time the PHP process can take to execute.
+     *
+     * @var integer
+     */
+    protected $timeout;
+
+    /**
      * @param Filesystem $filesystem
      * @param \PHPParser_Parser $parser
      */
@@ -34,6 +47,29 @@ class PhpWorker implements WorkerInterface
     {
         $this->filesystem = $filesystem;
         $this->parser     = $parser;
+
+        $this->setTimeout();
+        $this->setPrefix();
+    }
+
+    /**
+     * Sets the timeout in seconds.
+     *
+     * @param integer|float $timeout
+     */
+    public function setTimeout($timeout = 0)
+    {
+        $this->timeout = $timeout;
+    }
+
+    /**
+     * Sets the prefix that's used when creating a temporary directory.
+     *
+     * @param string $prefix
+     */
+    public function setPrefix($prefix = 'knpu_php_')
+    {
+        $this->prefix = $prefix;
     }
 
     /**
@@ -47,12 +83,26 @@ class PhpWorker implements WorkerInterface
         $result = new Result();
         $result->setInputFiles($inputFiles);
 
-        $process = $this->execute($inputFiles, $entryPoint);
+        try {
 
-        if ($process->isSuccessful()) {
-            $result->setOutput($process->getOutput());
-        } else {
-            $result->setLanguageError($process->getErrorOutput());
+            $process = $this->execute($inputFiles, $entryPoint);
+
+            if ($process->isSuccessful()) {
+                $result->setOutput($process->getOutput());
+            } else {
+                $result->setLanguageError($process->getErrorOutput());
+            }
+
+        } catch (RuntimeException $e) {
+            // A timeout is not an exceptional case. Since the validation
+            // errors would be overwritten by the asserter, the message has
+            // to be defined as a language error.
+
+            if (strpos($e->getMessage(), 'timed-out') !== false) {
+                $result->setLanguageError('It took too long time to execute your code.');
+            } else {
+                throw $e;
+            }
         }
 
         return $result;
@@ -92,15 +142,26 @@ class PhpWorker implements WorkerInterface
      * @param string $entryPoint
      *
      * @return Process  The process run; can be used to retrieve output
+     *
+     * @throws \Exception if the process fails
+     *
      */
     private function execute(Collection $files, $entryPoint)
     {
         $baseDir = $this->setUp($files, $this->filesystem);
 
         $process = $this->createProcess($baseDir, $entryPoint);
-        $process->run();
+        $process->setTimeout($this->timeout);
+
+        try {
+            $process->run();
+        } catch (\Exception $e) { }
 
         $this->tearDown($baseDir, $this->filesystem);
+
+        if (isset($e)) {
+            throw $e;
+        }
 
         return $process;
     }
@@ -118,7 +179,9 @@ class PhpWorker implements WorkerInterface
         $phpFinder = new PhpExecutableFinder();
         $php       = $phpFinder->find();
 
-        return new Process(sprintf('%s %s/%s', $php, $baseDir, $entryPoint));
+        // See http://symfony.com/doc/2.2/components/process.html#process-timeout
+        // for why exec is used here.
+        return new Process(sprintf('exec %s %s/%s', $php, $baseDir, $entryPoint));
     }
 
     /**
@@ -133,7 +196,7 @@ class PhpWorker implements WorkerInterface
     private function setUp(Collection $files, Filesystem $filesystem)
     {
         do {
-            $baseDir = sys_get_temp_dir().'/knpu_php_'.mt_rand();
+            $baseDir = sys_get_temp_dir().'/'.$this->prefix.mt_rand();
         } while (is_dir($baseDir));
 
         foreach ($files as $path => $contents) {

commit ddac43d1f788cce0ee9a5a97a225234a982c8a1b

Fixing broken worker.php service
This was introduced in 1ca2b27d

app/config/services.php
@@ -97,11 +97,14 @@ $app['worker.chained'] = $app->share(function ($app) {
 });
 
 $app['worker.php'] = $app->share(function ($app) {
-    return new KnpU\ActivityRunner\Worker\PhpWorker(
+    $worker = new KnpU\ActivityRunner\Worker\PhpWorker(
         $app['filesystem'],
-        $app['php_parser'],
-        $app['worker.time_limit']
+        $app['php_parser']
     );
+
+    $worker->setTimeout($app['worker.time_limit']);
+
+    return $worker;
 });
 
 $app['worker.twig'] = $app->share(function () {