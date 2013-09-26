b83783f5970fb55efb06b934106eafeb86f01bb7
Ryan Weaver <ryan@thatsquality.com>
2013-09-26 10:54:42 -0500

    Fixing and simplifying runner
    Yes, this bootstraps the Silex app, including controllers, so it's heavier, but it's easier: both the CLI and the web app use the same container. The CLI only cares that it's a Pimple container (where in reality it's also a Silex app), but that's ok

--- runner.php
+++ runner.php
@@ -1,15 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-require __DIR__.'/app/autoload.php';
-
 use KnpU\ActivityRunner\Console\Application;
 
-$configPath = __DIR__.'/app/config/';
-$paramsFile = $configPath.'parameters.php';
+$app = require(__DIR__.'/app/bootstrap.php');
 
-$pimple = require($configPath.'services.php');
-$pimple = require(is_file($paramsFile) ? $paramsFile : $paramsFile.'.dist');
-
-$application = new Application('UNKNOWN', 'UNKNOWN', $pimple);
+$application = new Application('UNKNOWN', 'UNKNOWN', $app);
 $application->run();
