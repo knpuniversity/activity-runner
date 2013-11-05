c4320f718eef5cd7bd88ffc564b10ef7328bd165
Ryan Weaver <ryan@thatsquality.com>
2013-11-05 09:47:41 -0600

    Updating note to be more accurate

--- src/KnpU/ActivityRunner/Configuration/ActivityConfigBuilder.php
+++ src/KnpU/ActivityRunner/Configuration/ActivityConfigBuilder.php
@@ -94,9 +94,8 @@ class ActivityConfigBuilder
             $rawConfig = $this->yaml->parse(file_get_contents($configPath));
             $rawConfig = $this->resolveRelativePaths($configBaseDir, $rawConfig);
 
-            // a bit hacky - KnpU needs to know the base dir in order to reverse and go back
-            // to the raw, non-absolute values. This really needs to be re-thought and we
-            // should be returning an object, not a big array
+            // send back the base_dir in case some more paths need to be made absolute
+            // todo - we should really return some configuration objects - the arrays and paths are tough to keep track of!
             foreach ($rawConfig as $key => $activityData) {
                 // store the base dir, which may be useful by whatever is using this
                 $rawConfig[$key]['base_dir'] = $configBaseDir;
