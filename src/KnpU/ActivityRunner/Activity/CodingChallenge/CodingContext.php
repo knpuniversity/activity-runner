2ea17747129c74bfc292d522b02a70f78ab9a5f7
Ryan Weaver <ryan@thatsquality.com>
2015-08-19 17:45:43 -0400

    Adding the ability to require files before execution
    For example, in Twig, you want to instantiate a Product object that's set
    to a variable. But before you do this in setupContext(), that file needs
    to be required.

--- src/KnpU/ActivityRunner/Activity/CodingChallenge/CodingContext.php
+++ src/KnpU/ActivityRunner/Activity/CodingChallenge/CodingContext.php
@@ -10,6 +10,18 @@ class CodingContext
     private $variables = array();
 
     /**
+     * The root directory where the files will be executed
+     *
+     * @var string
+     */
+    private $rootFileDir;
+
+    public function __construct($rootFileDir)
+    {
+        $this->rootFileDir = $rootFileDir;
+    }
+
+    /**
      * Add a variable that'll be made available to the scripts
      *
      * @param string $name
@@ -18,6 +30,20 @@ class CodingContext
     public function addVariable($name, $value)
     {
         $this->variables[$name] = $value;
+    }
+
+    /**
+     * Add a file that you want required before running the code
+     *
+     * The $localPath is the filename passed to the FileBuilder
+     *
+     *      $context->addRequire('Product.php');
+     *
+     * @param string $localPath
+     */
+    public function requireFile($localPath)
+    {
+        require $this->rootFileDir.'/'.$localPath;
     }
 
     public function getVariables()
